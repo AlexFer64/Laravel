ee553a0ed3e43509e0fcf057123adb5f72027537
Alexandre Ferreira <ferreira64240@hotmail.com>
2023-03-21 12:28:37 +0100

    tp en cours, ajout d'un formulaire, puis ajout de plusieurs champs dans les models, controllers

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'prenom',
+        'age',
         'name',
         'email',
+        'adresse',
         'password',
     ];
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('prenom');
+            $table->Integer('age');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('adresse');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
